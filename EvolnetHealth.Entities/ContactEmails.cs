67d3075c2b955d41f01b7c835162765d047f6127
RahulDhamangave <88035812+RahulDhamangave@users.noreply.github.com>
2021-07-27 17:26:40 +0530

    Added EvolnetHealth.Entities folder

---
+++ EvolnetHealth.Entities/ContactEmails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvolnetHealth.Entities
+{
+    public class ContactEmails
+    {
+        public int Id { get; set; }
+        public int? ContactId { get; set; }
+        public string Email { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
---
+++ EvolnetHealth.Entities/Contacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EvolnetHealth.Entities
+{
+    public class Contacts
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string LastName { get; set; }
+        public String Status { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumbers { get; set; }
+
+        
+
+    }
+}
